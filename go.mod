42aa0181bad850dcf0d76deb9d5174cce510292c
yuHsuan <x89301089@gmail.com>
2025-05-19 14:42:11 +0800

    [add] go dot env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Cyuhsuan/stray_map_back_end
 
 go 1.24.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
-// 1. 创建一个简单的HTTP服务器
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+var (
+	port = ":9000"
+)
+
+func init() {
+	_ = godotenv.Load()
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = ":" + envPort
+	}
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
-	http.ListenAndServe(":9000", nil)
+	http.ListenAndServe(port, nil)
 }
